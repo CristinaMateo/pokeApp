0804e282b34c7df4b214772cbbd5b251351ccbe0
Cristina Mateo <cris.mateo.99@gmail.com>
2023-12-05 19:23:19 +0100

    uppercase

--- src/components/Main/ListaPokemon/Card/Card.jsx
+++ src/components/Main/ListaPokemon/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ pokemon }) => {
 
   return (
     <div>
-      <h3>{pokemon.name}</h3>
+      <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
       <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       
     </div>
