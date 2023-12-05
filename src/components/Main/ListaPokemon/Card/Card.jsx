767980e227d721dae3706a5c4c65a6d4b3a205dc
Cristina Mateo <cris.mateo.99@gmail.com>
2023-12-05 19:28:11 +0100

    tipos

--- src/components/Main/ListaPokemon/Card/Card.jsx
+++ src/components/Main/ListaPokemon/Card/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({ pokemon }) => {
     <div>
       <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
       <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      
+      <p>{pokemon.types.map(tipo => <span> {tipo.type.name.toUpperCase()}</span>)}</p>
     </div>
   );
 };
